b8a6514f6a26617af52d00f8cb375cba522af16b
Gloria Marinelli <gloria.marinelli123@gmail.com>
2024-07-24 16:58:14 +0200

    some fix

--- server/prova.py
+++
@@ -1,9 +0,0 @@
-testo = "The Manhattan Transcripts Project, New York, New York, Introductory panel to Episode 1: The Park"
-testo1 = "The Manhattan Transcripts Project, New York, New York , Introductory panel to Episode 1: The Park"
-
-# Rimuovere gli spazi prima delle virgole
-pulito = testo.replace(' ,', ',')
-pulito1 = testo1.replace(' ,', ',')
-
-print(f"{pulito}")
-print(f"{pulito1}")
